d19f2ebc882fdcbd47da4988a343f7ed19e75b5b
Himanshu <hsyadav_b24@ee.vjti.ac.in>
2026-04-19 01:39:36 +0530

    third 3

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [jsxInJsPlugin(), react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
-      },
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
       },
     },
   },
